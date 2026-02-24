94eb7b3c68cb2d0246cc2e43707867027c4b6dba
Louisportier-24 <b2c.fact@gmail.com>
2026-02-24 14:17:27 +0100

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -26,4 +26,4 @@ export default async function handler(req, res) {
 
   const data = await notionRes.json();
   return res.status(notionRes.status).json(data);
-}
+};
